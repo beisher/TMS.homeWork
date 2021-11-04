cd8488d63418f69a5590f47e2f2e0b76a4bd7317
beish <beisher.vladislav@yandex.by>
2021-11-04 13:51:03 +0300

    deleted lesson3

--- src/com/tms/lesson3/tools/main.java
+++
@@ -1,8 +0,0 @@
-package com.tms.lesson3.tools;
-
-public class main {
-    public static void main(String[] args) {
-        System.out.println("HELLO WORLD");
-        System.out.println("my city ZHodino");
-    }
-}
--- src/com/tms/lesson3/tools/zadanie123.java
+++
@@ -1,7 +0,0 @@
-package com.tms.lesson3.tools;
-
-public class zadanie123 {
-    public static void main(String[] args) {
-        System.out.println("Hhfjdshfjsd");
-    }
-}
