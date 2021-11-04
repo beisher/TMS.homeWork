074de2c007a71bb6230493690dc07c2dc4649fdd
beish <beisher.vladislav@yandex.by>
2021-11-04 13:44:32 +0300

    add my city

--- src/com/tms/lesson3/tools/main.java
+++ src/com/tms/lesson3/tools/main.java
@@ -3,5 +3,6 @@ package com.tms.lesson3.tools;
 public class main {
     public static void main(String[] args) {
         System.out.println("HELLO WORLD");
+        System.out.println("my city ZHodino");
     }
 }
