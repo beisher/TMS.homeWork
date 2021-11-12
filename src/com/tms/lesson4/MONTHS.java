522f07a211524783bdd0e53469cccf3f6703e7a6
beish <beisher.vladislav@yandex.by>
2021-11-12 11:23:20 +0300

    Switch enum seasons

---
+++ src/com/tms/lesson4/MONTHS.java
@@ -0,0 +1,19 @@
+package com.tms.lesson4;
+
+public enum MONTHS {
+    December,
+    January,
+    February,
+
+    March,
+    April,
+    May,
+
+    June,
+    July,
+    August,
+
+    September,
+    October,
+    Novembe;
+}
