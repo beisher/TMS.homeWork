9678ed182c47ba0bc2d86a6a542aa12131b33ef9
beish <beisher.vladislav@yandex.by>
2021-11-13 18:46:34 +0300

    Lesson4 calling the constructor

---
+++ src/com/tms/lesson4/mainClassConstructors.java
@@ -0,0 +1,10 @@
+package com.tms.lesson4;
+
+public class mainClassConstructors {
+    public static void main(String[] args) {
+        classConstructors User = new classConstructors("Vladislav");
+        User.text();
+
+
+    }
+}
